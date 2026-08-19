7835c6d8ca797d44fd1a328abf495780feff891c
thienot <thienot280904@gmail.com>
2026-08-19 08:54:34 +0700

    set up cau truc spring boot

---
+++ bhxh-api/src/main/java/com/example/bhxh_api/BhxhApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bhxh_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhxhApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhxhApiApplication.class, args);
+	}
+
+}
